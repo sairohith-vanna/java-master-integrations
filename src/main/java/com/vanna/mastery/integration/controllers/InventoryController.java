a4d8c476542bb4755a85f12a0d94e6b7f2989d34
Sai Rohith Reddy Vangala <sairohit.vangala@vannainfotech.com>
2023-01-12 16:07:48 +0530

    Add a concrete service skeleton & updates

--- src/main/java/com/vanna/mastery/integration/controllers/InventoryController.java
+++ src/main/java/com/vanna/mastery/integration/controllers/InventoryController.java
@@ -1,10 +1,43 @@
 package com.vanna.mastery.integration.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.vanna.mastery.integration.models.ProductItem;
+import com.vanna.mastery.integration.services.InventoryManager;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("v1/inventory")
 public class InventoryController {
 
+    private final InventoryManager inventoryService;
+
+    public InventoryController(InventoryManager inventoryService) {
+        this.inventoryService = inventoryService;
+    }
+
+    @GetMapping("/")
+    @Operation(summary = "Get all the product items from inventory")
+    public ResponseEntity<List<ProductItem>> getProducts() {
+        var allProducts = inventoryService.getAllInventoryProductItems();
+        return ResponseEntity.ok(allProducts);
+    }
+
+    @PostMapping("/")
+    @Operation(summary = "Add a new product item to the inventory")
+    public ResponseEntity<ProductItem> addProduct(@RequestBody ProductItem newProductItem) {
+        var addedProduct = inventoryService.addProductToInventory(newProductItem);
+        return ResponseEntity.ok(addedProduct);
+    }
+
+    @GetMapping("{productId}")
+    @Operation(summary = "Get a specific product item from inventory")
+    public ResponseEntity<ProductItem> getProducts(@PathVariable("productId") UUID productId) {
+        var productItem = inventoryService.getProductItemDetails(productId);
+        return productItem.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
 }
--- src/main/java/com/vanna/mastery/integration/services/InventoryService.java
+++ src/main/java/com/vanna/mastery/integration/services/InventoryService.java
@@ -6,11 +6,31 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface InventoryService {
+@Service
+public class InventoryService implements InventoryManager{
+    
+    @Override
+    public List<ProductItem> getAllInventoryProductItems() {
+        return null;
+    }
 
-    List<ProductItem> getAllInventoryProductItems();
-    Optional<ProductItem> getProductItemDetails(UUID productId);
-    ProductItem addProductToInventory(ProductItem newProductItem);
-    UUID deleteProductItem(UUID deletableProductId);
-    ProductItem updateProductItem(UUID updatableProductId, ProductItem updateProductItemDetails);
+    @Override
+    public Optional<ProductItem> getProductItemDetails(UUID productId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public ProductItem addProductToInventory(ProductItem newProductItem) {
+        return null;
+    }
+
+    @Override
+    public UUID deleteProductItem(UUID deletableProductId) {
+        return null;
+    }
+
+    @Override
+    public ProductItem updateProductItem(UUID updatableProductId, ProductItem updateProductItemDetails) {
+        return null;
+    }
 }
